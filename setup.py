1becb329d742a16bf2a9edbaf8797f4514f9854c
miklosduma <miklos@sparkl.com>
2017-08-04 10:37:19 +0100

    restructured project folder, edited setup and added tests for message fun

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
 setup(
-    name="link_checker_tool",
-    version="1.0",
-    description="CLI tool that checks links in a ditamap",
-    author="Miklos Duma",
-    author_email="duma.miklos@gmail.com",
-    url="",
-    packages=[],
-    install_requires=['requests', 'termcolor']
+    name='dita_link_validator',
+    version='1.0',
+    description='CLI tool that checks html links in a ditamap',
+    author='Miklos Duma',
+    author_email='duma.miklos@gmail.com',
+    url='http://github.com/miklosduma/dita_link_validator',
+    packages=['dita_link_validator'],
+    license='MIT',
+    install_requires=['requests', 'termcolor'],
+    zip_safe=False,
+    test_suite='nose.collector',
+    tests_require=['nose']
 )
