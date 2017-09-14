7c0de117ffc5759e2437ba5b0f14917976649125
miklosduma <miklos@sparkl.com>
2017-09-14 12:34:11 +0100

    small modif and updated tests

--- dita_link_validator/link_checker.py
+++ dita_link_validator/link_checker.py
@@ -101,6 +101,9 @@ def links_map_checker(file):
     # Collect topicrefs with links from map
     topicrefs_with_links = get_topicrefs_from_map(file)
 
+    if topicrefs_with_links[0] == 'error':
+        return topicrefs_with_links
+
     # If no external links in topicrefs, return from function
     if len(topicrefs_with_links) == 0:
         print(console_message('warning', 'no_links_warn', file))
--- dita_link_validator/tests/test_map_checker.py
+++ dita_link_validator/tests/test_map_checker.py
@@ -5,24 +5,27 @@ from dita_link_validator import link_checker
 
 class TestMapChecker(TestCase):
 
-	# Testing with test ditamap. Should return all links in expected_error_links list
+        # Testing with test ditamap. Should return all links in
+        # expected_error_links list
     def test_check_map(self):
         check_file = 'dita_link_validator/tests/test_files/test_links.ditamap'
-        expected_error_links = ['http://no-such-link', 'missing_http_schema']
+        expected_error_links = [
+            'http://no-such-link', 'test_missing_http_schema', 'htp://test_invalid_protocol']
         (tag, message_key, error_links) = link_checker.links_map_checker(check_file)
         self.assertTrue(isinstance(error_links, list))
-        self.assertItemsEqual(expected_error_links,error_links)
+        self.assertItemsEqual(expected_error_links, error_links)
 
     # Testing with non-existent file. Should return no such file error
     def test_check_map_not_exist(self):
         check_file = 'no_such_file'
         result = link_checker.links_map_checker(check_file)
         expected = ('error', 'no_such_file_error', 'no_such_file')
-        self.assertItemsEqual(result,expected)
+        self.assertItemsEqual(result, expected)
 
     # Testing with file that is not xml. Should return not xml error
     def test_check_map_not_xml(self):
         check_file = 'dita_link_validator/messages.py'
         result = link_checker.links_map_checker(check_file)
-        expected = ('error', 'not_xml_error', 'dita_link_validator/messages.py')
-        self.assertItemsEqual(result,expected)
+        expected = ('error', 'not_xml_error',
+                    'dita_link_validator/messages.py')
+        self.assertItemsEqual(result, expected)
