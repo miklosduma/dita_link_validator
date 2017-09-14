17295fae97f4250377c471cb73b0a48c42ecbe47
miklosduma <miklos@sparkl.com>
2017-09-14 12:23:08 +0100

    added extra fun to check protocol, also split main fun into smaller chunks to be more managable

--- dita_link_validator/link_checker.py
+++ dita_link_validator/link_checker.py
@@ -7,10 +7,21 @@ import sys
 from messages import console_message
 
 
+def is_protocol_correct(link):
+    """
+    Correct protocols are https and http. Function returns False if start of link does not match either.
+    """
+    return link.startswith('http', 0, 4) or link.startswith('https', 0, 5)
+
+
 def check_link(link):
     """
     Pings link and sends back response tuple (tag,message_key) for console_message fun
     """
+
+    # Check protocol first. If incorrect, do not go further
+    if not is_protocol_correct(link):
+        return ('error', 'invalid_url_error')
 
     # Uses HEAD request to get status code
     try:
@@ -37,12 +48,19 @@ def check_link(link):
         return ('error', 'connection_error')
 
 
-def links_map_checker(file):
+def get_topicrefs_from_map(file):
     """
-    Expects a ditamap that has one or more topicrefs with href attributes.
-    Calls check_link function on all link values of hrefs
+    Collects topicref elements from ditamap.
+    Sample map:
+        <map>
+            ...
+            <topicref
+                href="http://docs.ansible.com/ansible/index.html"
+                ...
+            </topicref>
+            ...OTHER TOPICREFS...
+        </map>
     """
-
     print(console_message('info', 'check_message', file, with_tag=False))
 
     # Warns if name of file does not end with .ditamap
@@ -56,57 +74,15 @@ def links_map_checker(file):
         root_map = xml_tree.getroot()
         topicrefs = root_map.findall('topicref')
 
-        """
-        Sample map:
-        <map>
-            ...
-            <topicref
-                href="http://docs.ansible.com/ansible/index.html"
-                ...
-            </topicref>
-            ...OTHER TOPICREFS...
-        </map>
-        """
-
         # Checks only html links
         topicrefs_with_links = [
             x for x in topicrefs if x.attrib.get('format') == 'html']
-
-        # If no external links in topicrefs, return from function
-        if len(topicrefs_with_links) == 0:
-            print(console_message('warning', 'no_links_warn', file))
-            return ('warning', 'no_links_warn', file)
-
-        # Get links from topicrefs and ping them through check_link fun
-        # Collect broken links into error_links list
-        error_links = []
-        for topicref in topicrefs_with_links:
-            link = topicref.attrib.get('href')
-            (tag, message_key) = check_link(link)
-
-            # If pinging a link returns an error, link is added to error_links
-            # list
-            if tag == 'error':
-                error_links.append(link)
-                print(console_message(tag, message_key, link))
-
-            if tag == 'ok':
-                print(console_message(tag, message_key, link,
-                                      with_tag=False, with_color=False))
-
-        # If error_links holds any links, prints all links in it as a
-        # comma-separated string
-        if len(error_links) > 0:
-            print(console_message('error', 'error_count_message',
-                                  ", ".join(error_links), with_tag=False))
-            return ('error', 'error_count_message', error_links)
-
-        print(console_message('ok', 'all_good_message', file))
-        return ('ok', 'all_good_message', file)
+        return topicrefs_with_links
 
     # Returns exception if command argument (first positional) targets a
     # non-existent file
-    except IOError:
+    except IOError as e:
+        print(e)
         print(console_message('error', 'no_such_file_error', file))
         return ('error', 'no_such_file_error', file)
 
@@ -115,7 +91,50 @@ def links_map_checker(file):
         print(console_message('error', 'not_xml_error', file))
         return ('error', 'not_xml_error', file)
 
-# Call from command-line as 'python link_checker.py [PATH_TO_DITAMAP]'
+
+def links_map_checker(file):
+    """
+    Expects a ditamap that has one or more topicrefs with href attributes.
+    Calls check_link function on all link values of hrefs
+    """
+
+    # Collect topicrefs with links from map
+    topicrefs_with_links = get_topicrefs_from_map(file)
+
+    # If no external links in topicrefs, return from function
+    if len(topicrefs_with_links) == 0:
+        print(console_message('warning', 'no_links_warn', file))
+        return ('warning', 'no_links_warn', file)
+
+    # Get links from topicrefs and ping them through check_link fun
+    # Collect broken links into error_links list
+    error_links = []
+
+    for topicref in topicrefs_with_links:
+
+        link = topicref.attrib.get('href')
+        (tag, message_key) = check_link(link)
+
+        # If pinging a link returns an error, link is added to error_links
+        if tag == 'error':
+            error_links.append(link)
+            print(console_message(tag, message_key, link))
+
+        if tag == 'ok':
+            print(console_message(tag, message_key, link,
+                                  with_tag=False, with_color=False))
+
+    # If error_links holds any links, prints all links in it as a
+    # comma-separated string
+    if len(error_links) > 0:
+        print(console_message('error', 'error_count_message',
+                              ", ".join(error_links), with_tag=False))
+        return ('error', 'error_count_message', error_links)
+
+    print(console_message('ok', 'all_good_message', file))
+    return ('ok', 'all_good_message', file)
+
+
 if __name__ == "__main__":
     # Only calls command if minimum one argument is specified (command itself
     # is an element in sys.argv list)
