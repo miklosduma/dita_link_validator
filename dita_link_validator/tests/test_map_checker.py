6bab9a6d5d3c6c09c3d4d4c71adb8cb30a05d320
miklosduma <miklos@sparkl.com>
2017-09-21 11:04:43 +0100

    updated tests and linting

--- dita_link_validator/tests/test_map_checker.py
+++ dita_link_validator/tests/test_map_checker.py
@@ -1,39 +1,71 @@
-from unittest import TestCase
+"""
+Test cases for collecting and checking links
+in case of structured ditamap.
+"""
 
-from dita_link_validator import link_checker
+from unittest import TestCase
+from dita_link_validator.check_links_dita import links_map_checker
 
 
 class TestMapChecker(TestCase):
+    """
+    Class for ditamap check tests.
+    """
 
-        # Testing with test ditamap. Should return all links in
-        # expected_error_links list
+    test_files_dir = 'dita_link_validator/tests/test_files'
+
     def test_check_map(self):
-        check_file = 'dita_link_validator/tests/test_files/test_links.ditamap'
+        """
+        Testing with test ditamap.
+        Expects: ('error', 'error_count_message', error_links)
+        error_links must include all links in expected_error_links list.
+        """
+        check_file = '%s/test_links.ditamap' % (TestMapChecker.test_files_dir)
+
+        expected_tag = 'error'
+        expected_message_key = 'error_count_message'
         expected_error_links = [
-            'http://no-such-link', 'test_missing_http_schema', 'htp://test_invalid_protocol']
-        (tag, message_key, error_links) = link_checker.links_map_checker(check_file)
+            'http://no-such-link',
+            'test_missing_http_schema',
+            'htp://test_invalid_protocol']
+
+        (tag,
+         message_key,
+         error_links) = links_map_checker(check_file)
         self.assertTrue(isinstance(error_links, list))
+        self.assertItemsEqual(expected_tag, tag)
+        self.assertItemsEqual(expected_message_key, message_key)
         self.assertItemsEqual(expected_error_links, error_links)
 
-    # Testing with non-existent file. Should return no such file error
     def test_check_map_not_exist(self):
+        """
+        Testing with non-existent file.
+        Must return no such file error.
+        """
         check_file = 'no_such_file'
-        result = link_checker.links_map_checker(check_file)
+        result = links_map_checker(check_file)
         expected = ('error', 'no_such_file_error', 'no_such_file')
         self.assertItemsEqual(result, expected)
 
-    # Testing with file that is not xml. Should return not xml error
     def test_check_map_not_xml(self):
+        """
+        Testing with file that is not xml.
+        Must return not xml error.
+        """
         check_file = 'dita_link_validator/messages.py'
-        result = link_checker.links_map_checker(check_file)
+        result = links_map_checker(check_file)
         expected = ('error', 'not_xml_error',
                     'dita_link_validator/messages.py')
         self.assertItemsEqual(result, expected)
 
-    # Testing with map that has no external links
     def test_map_with_no_link(self):
-        check_file = 'dita_link_validator/tests/test_files/test_map_no_links.ditamap'
-        result = link_checker.links_map_checker(check_file)
+        """
+        Testing with map that has no external links.
+        Must return no links warning.
+        """
+        check_file = '%s/test_map_no_links.ditamap' % (
+            TestMapChecker.test_files_dir)
+        result = links_map_checker(check_file)
         expected = ('warning', 'no_links_warn',
-                    'dita_link_validator/tests/test_files/test_map_no_links.ditamap')
+                    check_file)
         self.assertItemsEqual(result, expected)
--- dita_link_validator/tests/test_messages.py
+++ dita_link_validator/tests/test_messages.py
@@ -1,21 +1,40 @@
+"""
+Test cases for messages.
+"""
+
 from unittest import TestCase
 
 from dita_link_validator import messages
 
 
 class TestMessage(TestCase):
+    """
+    Class for message tests.
+    """
 
-        # Checking success message for running the script on a correct file (no
-        # colors)
     def test_message_1(self):
+        """
+        Checking success message for running the script
+        on a correct file (no colors)
+        """
+
         result = messages.console_message(
-            'ok', 'check_message', 'file_name', with_color=False)
+            'ok',
+            'check_message',
+            'file_name',
+            with_color=False)
         expected = 'SUCCESS!!!!! Checking links in file: file_name'
         self.assertEqual(result, expected)
 
-    # Checking status code error message (no colors, no tags)
     def test_message_2(self):
-        result = messages.console_message(
-            'error', 'status_code_error', 'link_name', with_color=False, with_tag=False)
+        """
+        Checking status code error message (no colors, no tags).
+        """
+
+        result = messages.console_message('error',
+                                          'status_code_error',
+                                          'link_name',
+                                          with_color=False,
+                                          with_tag=False)
         expected = 'Browser sent back error status code. Check link: link_name'
         self.assertEqual(result, expected)
