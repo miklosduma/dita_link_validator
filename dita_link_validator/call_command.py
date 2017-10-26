3ff2f5ca8f889d83c6bb6df4240bd08fdbc0e6c7
miklosduma <miklos@sparkl.com>
2017-10-26 17:34:50 +0100

    started replacing main module with argparse

--- dita_link_validator/call_command.py
+++ dita_link_validator/call_command.py
@@ -1,16 +1,41 @@
 from __future__ import print_function
-import sys
+import argparse
 
 from messages import console_message
 from check_links_dita import links_map_checker
 from check_links_markdown import check_links_in_dir
 
 
+def main():
+    """
+    Passes command-line arguments to the call_main function.
+    """
+    desc = 'Command-line link validator tool.'
+    
+
+    parser = argparse.ArgumentParser(description=desc)
+
+    # Add verbose True or False based on whether -v flag is specified
+    parser.add_argument('-dt', '--dita')
+
+    # Add phases in case one or more are specified
+    parser.add_argument('-md', '--markdown')
+
+    # Get command-line arguments
+    args = parser.parse_args()
+    if args.dita:
+        return links_map_checker(args.dita)
+    if args.markdown:
+        return check_links_in_dir(args.markdown)
+
+    return parser.print_help()
+
+"""
 def call_main(args):
-    """
-    Main function to call either dita or markdown
-    link checker funs.
-    """
+    
+    #Main function to call either dita or markdown
+    #link checker funs.
+    
 
     # First positional arguments
     valid_options = ['dita', 'markdown']
@@ -39,7 +64,8 @@ def call_main(args):
     # Second arg must be a directory. This is handled in check_links_in_dir
     if command_option == 'markdown':
         return check_links_in_dir(command_target)
-
+"""
 # Call from command line. Args are gathered from terminal input
 if __name__ == "__main__":
-    call_main(sys.argv)
+    #call_main(sys.argv)
+    main()
